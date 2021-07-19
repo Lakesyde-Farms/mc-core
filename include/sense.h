416fc67d787b3c31be9c36c7c33001a162bdc06d
AndrewGlago <andrewglago1@gmail.com>
2021-07-19 20:36:50 +0000

    ota initial support (still untested)

--- include/sense.h
+++ include/sense.h
@@ -1,5 +1,8 @@
+#include <Arduino.h>
+
 #ifndef sense_H
 #define sense_H
+#pragma once
 
 class SensorCL {
   public: 
--- src/sense.cpp
+++ src/sense.cpp
@@ -1,26 +1,26 @@
+#include <Arduino.h>
 #include <sense.h>
 #include <DHT.h>
 
-#define DHTPIN 2
+#define DHTPIN 1
 #define DHTTYPE DHT22
-
-uint8_t A3;
-uint8_t A0;
 
 DHT dht = DHT(DHTPIN, DHTTYPE);
 
-class SensorCL {
-  public:
-    float tempSense(){
-      return dht.readTemperature();
-    };
-    float humidSense(){
-      return dht.readHumidity();
-    };
-    float lightSense(){
-      return analogRead(A0);
-    };
-    float moistureSense(){
-      return analogRead(A3);
-    };
+float SensorCL::tempSense() {
+  return dht.readTemperature();
 };
+
+float SensorCL::humidSense(){
+  return dht.readHumidity();
+};
+float SensorCL::lightSense(){
+  return analogRead(A0);
+};
+// float SensorCL::moistureSense(){
+//   return analogRead(A0);
+// };
+
+
+
+
